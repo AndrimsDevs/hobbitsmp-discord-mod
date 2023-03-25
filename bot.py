6003cc48a79a7eb162b28c83c7ada79cf9754f98
Ault <ault@andrims.com>
2023-03-25 04:57:18 -0400

    tried to send msg in separate channel
    didnt work. maybe need to try using webhooks in py with json embeds to send notif to flags channel

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 # base example code taken from discord.py docs
 
 import discord
+from discord.ext import commands
+from discord import Permissions
+
+description = ""
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="TBD", description=description, intents=intents)
 
 forbidden_words = ["fuck", "shit", "bitch", "whore", "nigga", "nigger", "cunt", "pussy", "fag"]
 
@@ -9,14 +18,20 @@ class MyClient(discord.Client):
         await client.change_presence(status=discord.Status.dnd, activity=discord.Game('Watching Ault and Bowswa\'s msgs'))
         print(f'Logged on as {self.user}!')
 
+    @bot.event
     async def on_message(self, message):
+        # to make sure bot does not reply to itself
+        if message.author.id == self.user.id:
+            return
+        
         for bannedWord in forbidden_words:
             if (str(message.content)).find(bannedWord) != -1:
                 print(f'Message from {message.author}: {message.content}')
+                await ctx.send(f"{message.author} watch your language.")
+                # await message.reply(f"{message.author} watch your language.", mention_author=True)
                 break
 
-intents = discord.Intents.default()
-intents.message_content = True
+
 
 client = MyClient(intents=intents)
 client.run('TOKEN')
