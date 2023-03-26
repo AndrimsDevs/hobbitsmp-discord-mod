6b853c6be127d89410390fadc5d90a5dd5661eeb
Ault <ault@andrims.com>
2023-03-25 23:19:49 -0400

    Finished flagging system
    added comments describing everything. flagging system no longer picks up on it's own webhook because transferred to embeds.

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# base example code taken from discord.py docs
 
 import discord
 from discord.ext import commands
@@ -6,26 +5,29 @@ from discord import Permissions
 
 description = ""
 
+# declare intents to discord api
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="TBD", description=description, intents=intents)
 
+# forbidden words list
 forbidden_words = ["fuck", "shit", "bitch", "whore", "nigga", "nigger", "cunt", "pussy", "fag"]
 
-def notifDiscord(wordToRaise, messageAuthor, messageLink):
+# function to send discord a flag
+def raiseFlag(wordToRaise, messageAuthor, messageLink):
     import requests
 
     # Define the webhook URL and payload data
     webhook_url = "https://discord.com/api/webhooks/1089343268782882956/k7FwrQirM_BP_WSAxwStVMnGGa44LHgnJUV3uBj92nfHJq0xCLoNXCy0yBdbxDUUqAyt"
     payload = {
         "content": "Flag Inbound",
-        "username": "Owner Word Flagging",
+        "username": "Owner Banned Word Flagging",
         "avatar_url": "https://my-bot-avatar-image-url.com/avatar.png",
         "embeds": [
             {
                 "title": "flag ig",
-                "description": "test",
+                "description": "%s sent flag key: ||`%s`|| in <#%s>"%(messageAuthor, wordToRaise, messageLink),
                 "color": 16711680, # RGB color code (decimal) - red in this example
             }
         ]
@@ -35,33 +37,34 @@ def notifDiscord(wordToRaise, messageAuthor, messageLink):
     response = requests.post(webhook_url, json=payload)
 
     # Check the response status code to see if the webhook was successful
-    if response.status_code == 200:
+    if response.status_code == 204:
         print("Webhook sent successfully!")
     else:
         print("Error sending webhook:", response.status_code)
 
-
 class MyClient(discord.Client):
-    async def on_ready(self):
+    # sets discord prescense on script boot
+    async def on_ready(self): 
         await client.change_presence(status=discord.Status.dnd, activity=discord.Game('Watching Ault and Bowswa\'s msgs'))
         print(f'Logged on as {self.user}!')
 
-    @bot.event
+    @bot.event # executes code below when message detected by bot in guild
     async def on_message(self, message):
         # to make sure bot does not reply to itself
-        if message.author.id == self.user.id or message.author.id == "1089343268782882956":
-            return
-        
-        if message.channel.id == "1089046162956370040":
+        if message.author.id == self.user.id:
             return
         
         for bannedWord in forbidden_words:
             if (str(message.content)).find(bannedWord) != -1:
+                # log into console
                 print(f'Message from {message.author}: {message.content}')
-                # await ctx.send(f"{message.author} watch your language.")
-                notifDiscord(bannedWord, message.author, message.content)
+                # send notification embed to discord
+                raiseFlag(bannedWord, message.author, message.channel.id)
+                # reply to message sender
                 await message.reply(f"{message.author} watch your language.", mention_author=True)
+                # break out of loop to avoid multiple flags for multiple flags within one msg
                 break
 
+# run bot with declared intents
 client = MyClient(intents=intents)
-client.run('TOKEN')
+client.run('YOUR TOKEN HERE')
